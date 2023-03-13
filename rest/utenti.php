6d6a975f8f51618ea65134946bdc092c18c2d1b1
Riccardo Riggi <ghirowinformatica@gmail.com>
2023-03-13 22:05:53 +0100

    Implementati servizi per gestione utenti

---
+++ rest/utenti.php
@@ -0,0 +1,145 @@
+<?php
+
+include './importManager.php';
+include '../services/utentiService.php';
+
+try {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Expose-Headers: *');
+    header('Access-Control-Max-Age: 86400');
+    if (strtolower($_SERVER['REQUEST_METHOD']) == 'options')
+        exit();
+
+
+    if (!isset($_GET["nomeMetodo"]))
+        throw new ErroreServerException("Non è stato fornito il riferimento del metodo da invocare");
+
+
+    if ($_GET["nomeMetodo"] == "getListaUtenti") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "GET")
+            throw new MetodoHttpErratoException();
+
+        if (!isset($_GET["pagina"]))
+            throw new OtterGuardianException(400, "Il campo pagina è richiesto");
+
+
+        $response = getListaUtenti($_GET["pagina"]);
+        http_response_code(200);
+        exit(json_encode($response));
+    } else if ($_GET["nomeMetodo"] == "inserisciUtente") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "POST")
+            throw new MetodoHttpErratoException();
+
+        $jsonBody = json_decode(file_get_contents('php://input'), true);
+
+        if (!isset($jsonBody["nome"]))
+            throw new OtterGuardianException(400, "Il campo nome è richiesto");
+
+        if (!isset($jsonBody["cognome"]))
+            throw new OtterGuardianException(400, "Il campo cognome è richiesto");
+
+        if (!isset($jsonBody["email"]))
+            throw new OtterGuardianException(400, "Il campo email è richiesto");
+
+        if (!isset($jsonBody["password"]))
+            throw new OtterGuardianException(400, "Il campo password è richiesto");
+
+        if (!isset($jsonBody["confermaPassword"]))
+            throw new OtterGuardianException(400, "Il campo confermaPassword è richiesto");
+
+        if ($jsonBody["confermaPassword"] !== $jsonBody["password"]) {
+            throw new OtterGuardianException(400, "Il campo password deve essere uguale al campo confermaPassword");
+        }
+
+        inserisciUtente($jsonBody["nome"], $jsonBody["cognome"], $jsonBody["email"], $jsonBody["password"]);
+        http_response_code(200);
+    } else if ($_GET["nomeMetodo"] == "modificaUtente") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "PUT")
+            throw new MetodoHttpErratoException();
+
+        if (!isset($_GET["idUtente"]))
+            throw new OtterGuardianException(400, "Il campo idUtente è richiesto");
+
+
+        $jsonBody = json_decode(file_get_contents('php://input'), true);
+
+        if (!isset($jsonBody["nome"]))
+            throw new OtterGuardianException(400, "Il campo nome è richiesto");
+
+        if (!isset($jsonBody["cognome"]))
+            throw new OtterGuardianException(400, "Il campo cognome è richiesto");
+
+
+        $response = modificaUtente($jsonBody["nome"], $jsonBody["cognome"], $_GET["idUtente"]);
+        http_response_code(200);
+    } else if ($_GET["nomeMetodo"] == "eliminaUtente") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "DELETE")
+            throw new MetodoHttpErratoException();
+
+        if (!isset($_GET["idUtente"]))
+            throw new OtterGuardianException(400, "Il campo idUtente è richiesto");
+
+
+
+        eliminaUtente($_GET["idUtente"]);
+        http_response_code(200);
+    } else if ($_GET["nomeMetodo"] == "getUtente") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "GET")
+            throw new MetodoHttpErratoException();
+
+        if (!isset($_GET["idUtente"]))
+            throw new OtterGuardianException(400, "Il campo idUtente è richiesto");
+
+
+        $response = getUtente($_GET["idUtente"]);
+        http_response_code(200);
+        exit(json_encode($response));
+    } else if ($_GET["nomeMetodo"] == "bloccaUtente") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "PUT")
+            throw new MetodoHttpErratoException();
+
+        if (!isset($_GET["idUtente"]))
+            throw new OtterGuardianException(400, "Il campo idUtente è richiesto");
+
+
+        bloccaUtente($_GET["idUtente"]);
+        http_response_code(200);
+    } else if ($_GET["nomeMetodo"] == "sbloccaUtente") {
+
+        if ($_SERVER['REQUEST_METHOD'] != "PUT")
+            throw new MetodoHttpErratoException();
+
+        if (!isset($_GET["idUtente"]))
+            throw new OtterGuardianException(400, "Il campo idUtente è richiesto");
+
+        sbloccaUtente($_GET["idUtente"]);
+        http_response_code(200);
+    } else {
+        throw new OtterGuardianException(500, "Metodo non implementato");
+    }
+} catch (AccessoNonAutorizzatoLoginException $e) {
+    httpAccessoNonAutorizzatoLogin();
+} catch (AccessoNonAutorizzatoException $e) {
+    httpAccessoNonAutorizzato();
+} catch (MetodoHttpErratoException $e) {
+    httpMetodoHttpErrato();
+} catch (ErroreServerException $e) {
+    httpErroreServer($e->getMessage());
+} catch (OtterGuardianException $e) {
+    http_response_code($e->getStatus());
+    $oggetto = new stdClass();
+    $oggetto->codice = $e->getStatus();
+    $oggetto->descrizione = $e->getMessage();
+    exit(json_encode($oggetto));
+} catch (Exception $e) {
+    generaLogSuFile("Errore sconosciuto: " . $e->getMessage());
+    httpErroreServer("Errore sconosciuto");
+}
---
+++ services/utentiService.php
@@ -0,0 +1,231 @@
+<?php
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: getListaUtenti
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('getListaUtenti')) {
+    function getListaUtenti($pagina)
+    {
+        //verificaValiditaToken();
+        $paginaDaEstrarre = ($pagina - 1) * ELEMENTI_PER_PAGINA;
+
+        $sql = "SELECT idUtente, nome, cognome, email, dataBlocco FROM " . PREFISSO_TAVOLA . "_utenti WHERE dataEliminazione IS NULL ORDER BY idUtente LIMIT :pagina, " . ELEMENTI_PER_PAGINA;
+
+        $conn = apriConnessione();
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':pagina', $paginaDaEstrarre, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        $array = [];
+        foreach ($result as $value) {
+            $tmp = $value;
+            $tmp["nome"] = decifraStringa($value["nome"]);
+            $tmp["cognome"] = decifraStringa($value["cognome"]);
+            $tmp["email"] = decifraStringa($value["email"]);
+
+
+            $tmp["1"] = decifraStringa($value["1"]);
+            $tmp["2"] = decifraStringa($value["2"]);
+            $tmp["3"] = decifraStringa($value["3"]);
+
+            array_push($array, $tmp);
+        }
+        return $array;
+    }
+}
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: inserisciUtente
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('inserisciUtente')) {
+    function inserisciUtente($nomeInChiaro, $cognomeInChiaro, $emailInChiaro, $passwordInChiaro)
+    {
+        verificaValiditaToken();
+
+        $nome = cifraStringa($nomeInChiaro);
+        $cognome = cifraStringa($cognomeInChiaro);
+        $email = cifraStringa($emailInChiaro);
+        $password = md5(md5($passwordInChiaro));
+
+        $sql = "INSERT INTO " . PREFISSO_TAVOLA . "_utenti (nome, cognome, email, password, dataCreazione) VALUES (:nome, :cognome, :email, :password, current_timestamp )";
+
+
+        $conn = apriConnessione();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':cognome', $cognome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+
+        $idUtenteInserito = $conn->lastInsertId();
+        associaRuoloUtenteDefault($idUtenteInserito);
+        abilitaTipoRecuperoPasswordDefault($idUtenteInserito);
+        abilitaTipoMetodoLoginDefault($idUtenteInserito);
+    }
+}
+
+if (!function_exists('abilitaTipoRecuperoPasswordDefault')) {
+    function abilitaTipoRecuperoPasswordDefault($idUtente)
+    {
+
+        $conn = apriConnessione();
+        $stmt = $conn->prepare("INSERT INTO " . PREFISSO_TAVOLA . "_metodi_rec_psw (idUtente, idTipoMetodoRecPsw, dataInizioValidita) VALUES (:idUtente, 'REC_PSW_EMAIL_SIX_APP', current_timestamp)");
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+    }
+}
+
+if (!function_exists('associaRuoloUtenteDefault')) {
+    function associaRuoloUtenteDefault($idUtente)
+    {
+
+        $sql = "INSERT INTO " . PREFISSO_TAVOLA . "_ruoli_utenti (idTipoRuolo, idUtente, dataCreazione) VALUES ('USER', :idUtente, current_timestamp )";
+
+
+        $conn = apriConnessione();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+    }
+}
+
+if (!function_exists('abilitaTipoMetodoLoginDefault')) {
+    function abilitaTipoMetodoLoginDefault($idUtente)
+    {
+
+        $conn = apriConnessione();
+        $stmt = $conn->prepare("INSERT INTO " . PREFISSO_TAVOLA . "_metodi_login (idUtente, idTipoMetodoLogin, dataInizioValidita) VALUES (:idUtente, 'EMAIL_PSW_SIX_EMAIL', current_timestamp)");
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: getUtente
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('getUtente')) {
+    function getUtente($idUtente)
+    {
+        verificaValiditaToken();
+
+        $sql = "SELECT idUtente, nome, cognome, email, dataBlocco FROM " . PREFISSO_TAVOLA . "_utenti WHERE dataEliminazione IS NULL AND idUtente = :idUtente";
+
+        $conn = apriConnessione();
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        $array = [];
+        foreach ($result as $value) {
+            $tmp = $value;
+            $tmp["nome"] = decifraStringa($value["nome"]);
+            $tmp["cognome"] = decifraStringa($value["cognome"]);
+            $tmp["email"] = decifraStringa($value["email"]);
+
+
+            $tmp["1"] = decifraStringa($value["1"]);
+            $tmp["2"] = decifraStringa($value["2"]);
+            $tmp["3"] = decifraStringa($value["3"]);
+
+            array_push($array, $tmp);
+        }
+        return $array;
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: modificaUtente
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('modificaUtente')) {
+    function modificaUtente($nomeInChiaro, $cognomeInChiaro, $idUtente)
+    {
+        verificaValiditaToken();
+
+        $nome = cifraStringa($nomeInChiaro);
+        $cognome = cifraStringa($cognomeInChiaro);
+
+        $sql = "UPDATE " . PREFISSO_TAVOLA . "_utenti SET nome= :nome, cognome= :cognome WHERE idUtente = :idUtente AND dataEliminazione IS NULL ";
+
+
+        $conn = apriConnessione();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':cognome', $cognome);
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: eliminaUtente
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('eliminaUtente')) {
+    function eliminaUtente($idUtente)
+    {
+        
+        verificaValiditaToken();
+
+        $sql = "UPDATE " . PREFISSO_TAVOLA . "_utenti SET dataEliminazione = current_timestamp WHERE idUtente = :idUtente AND dataEliminazione IS NULL ";
+
+        $conn = apriConnessione();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: bloccaUtente
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('bloccaUtente')) {
+    function bloccaUtente($idUtente)
+    {
+        
+        verificaValiditaToken();
+
+        $sql = "UPDATE " . PREFISSO_TAVOLA . "_utenti SET dataBlocco = current_timestamp WHERE idUtente = :idUtente AND dataEliminazione IS NULL and dataBlocco IS NULL";
+
+        $conn = apriConnessione();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Funzione: sbloccaUtente
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+if (!function_exists('sbloccaUtente')) {
+    function sbloccaUtente($idUtente)
+    {
+        
+        verificaValiditaToken();
+
+        $sql = "UPDATE " . PREFISSO_TAVOLA . "_utenti SET dataBlocco = NULL WHERE idUtente = :idUtente AND dataEliminazione IS NULL and dataBlocco IS NOT NULL";
+
+        $conn = apriConnessione();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':idUtente', $idUtente);
+        $stmt->execute();
+
+    }
+}
